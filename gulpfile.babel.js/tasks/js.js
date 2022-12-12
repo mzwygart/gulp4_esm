4ad5be2d8f9074519552c117cb0620d6e5524c3a
Mathieu Zwygart <mathieu@zwygart.net>
2022-12-12 23:43:07 +0100

    Basic webpack support

--- gulpfile.babel.js/tasks/js.js
+++ gulpfile.babel.js/tasks/js.js
@@ -1,20 +1,12 @@
 import webpackStream from 'webpack-stream'
-import webpack from 'webpack'
 
 const js = () => {
   return $.gulp
-    .src($.path.js.src, { sourcemaps: $.app.isDev })
-    .pipe(
-      $.gp.plumber({
-        errorHandler: $.gp.notify.onError((error) => ({
-          title: 'JS',
-          message: error.message,
-        })),
-      })
-    )
-    .pipe($.gp.babel())
-    .pipe(webpackStream($.app.webpack, webpack))
-    .pipe($.gulp.dest($.path.js.dest, { sourcemaps: $.app.isDev }))
+    .src('./src/js/main.js')
+    .pipe(webpackStream({
+      mode: $.app.isProd ? 'production' : 'development'
+    }))
+    .pipe($.gulp.dest('./public/js'))
     .pipe($.browserSync.stream())
 }
 
